589d9e23e359688cd49b96cb6b918268f323a9d3
namjin <skawls25@gmail.com>
2024-01-08 01:09:30 +0900

    [feat] init runner

---
+++ src/main/java/com/lx/board/init/InitRunner.java
@@ -0,0 +1,20 @@
+package com.lx.board.init;
+
+import com.lx.board.domain.member.application.port.out.MemberPersistent;
+import com.lx.board.domain.member.domain.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class InitRunner implements ApplicationRunner {
+
+    private final MemberPersistent memberPersistent;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        memberPersistent.save(Member.create("test", "test", "test"));
+    }
+}
